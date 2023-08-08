a4b58ed3bd57d7d45be029cd0ccb452ec2934ad6
EMPLOYEE <EMPLOYEE@terasys-16Z90P>
2023-08-08 16:59:33 +0900

    sql test

--- ex00/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex00/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,7 +7,17 @@ import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("select * from tbl_board where bno > 0")
+	//@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
 	
 }
--- ex00/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex00/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -13,13 +14,52 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class BoardMapperTests {
-	
-	@Setter(onMethod_ = @Autowired)
-	private BoardMapper mapper;
-	
-	@Test
-	public void testGetList() {
-		mapper.getList().forEach(board -> log.info(board));
-	}
 
+  @Setter(onMethod_ = @Autowired)
+  private BoardMapper mapper;
+
+
+  @Test 
+  public void testGetList() {
+    mapper.getList().forEach(board ->log.info(board)); 
+  }
+
+  /*
+   * @Test public void tesetInsertSelectKey() { BoardVO board = new BoardVO();
+   * board.setTitle("새로 작성하는 글"); board.setContent("새로 작성하는 내용");
+   * board.setWriter("newbie");
+   * 
+   * mapper.insertSelectKey(board);
+   * 
+   * log.info(board);
+   * 
+   * testGetList(); }
+   */
+
+  @Test
+  public void testRead() {
+    BoardVO board = mapper.read(5L);
+
+    log.info(board);
+  }
+
+  /*
+   * @Test public void testDelete() {
+   * log.info("DELETE COUNT: "+mapper.delete(3L)); }
+   */
+
+  @Test
+  public void testUpdate() {
+    BoardVO board = new BoardVO();
+
+    board.setBno(5L);
+    board.setTitle("수정된제목");
+    board.setContent("수정된내용");
+    board.setWriter("user00");
+
+    int count = mapper.update(board);
+    log.info("UPDATE COUNT: " + count);
+
+    testGetList();
+  }
 }
